c0d01e6666010edcf64f5791ce031c5233107166
castrokimaru <castrokimaru.com>
2025-10-22 16:08:32 +0300

    layout

--- src/LayOut.jsx
+++ src/LayOut.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ToolBar from './ToolBar';
+import ToolBar from './components/ToolBar';
 import {Outlet} from 'react-router-dom';
 
  function LayOut() {
     return (
         <div classname="flex min-h-screen  bg-gray-50">
         <ToolBar />
-        <div classname="flex-1 p-8 ">
+        <div classname="flex-1 p-8">
           <Outlet /> 
         </div>
         </div>
