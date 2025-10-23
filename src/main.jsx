4591563c6b532c6922921e77a07b5bd6a5f211c6
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-10-24 02:24:46 +0300

    main-jsx-react-reconciliation

--- src/main.jsx
+++ src/main.jsx
@@ -2,57 +2,37 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ToolBar from './ToolBar.jsx'
 import About from './pages/About.jsx'
-
-import Home from './pages/Home.jsx' 
+import Home from './pages/Home.jsx'
 import Layout from './LayOut.jsx'
-
 import Appointments from './pages/Appointments.jsx'
 import DoctorAv from './pages/DoctorAv.jsx'
-
 import DoctorsSpeciality from './pages/DoctorsSpeciality.jsx'
 import ServicesCatalog from './pages/Services.jsx'
-
 import PatientRecords from './pages/Records.jsx'
 
-
-
-const router = createBrowserRouter ([ 
-   {
+const router = createBrowserRouter([
+  {
     path: "/",
-    element: <Layout />, // this wraps all your pages with ToolBar
-    children: [                      // the array of objects store the different routes
-  {path:"/", 
-  element:<Home/>},
-  {path:"/home",
-  element:<Home />},
-  {path:"/services",
-  element:<ServicesCatalog /> },
-  {path:"/about",
-  element:<About />},
-  {path:"/appointments",
-  element:<Appointments />},
-  {path:"/doctors",
-  element:<DoctorsSpeciality />},
-  {path:"/availability",
-  element:<DoctorAv />},
-<<<<<<< Updated upstream
-=======
-  {path:"/records",
-  element:<PatientRecords />},
-  {path:"/billing",
-  element:<ToolBar />},
->>>>>>> Stashed changes
-  ]
-  }
-  
-
+    element: <Layout />, // Wraps all pages with the main layout
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/home", element: <Home /> },
+      { path: "/services", element: <ServicesCatalog /> },
+      { path: "/about", element: <About /> },
+      { path: "/appointments", element: <Appointments /> },
+      { path: "/doctors", element: <DoctorsSpeciality /> },
+      { path: "/availability", element: <DoctorAv /> },
+      { path: "/records", element: <PatientRecords /> },
+      { path: "/billing", element: <ToolBar /> },
+    ],
+  },
 ]);
 
-createRoot(document.getElementById('root')).render(              //pointing to the root div in index.html
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <RouterProvider router={router} />                              //providing the router to the app
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
